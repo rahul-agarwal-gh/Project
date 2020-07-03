988c73b97f8498d2fe99487c4ffcbdb454f7e52a
Rahul Agarwal <rahulagarwal1809@gmail.com>
2020-07-03 11:26:03 +0530

    Added session handling code using passport

---
+++ greeting.js
@@ -0,0 +1,6 @@
+const goodGreeting = "Hello there matey!";
+const badGreeting = "Dafuq you doin here!";
+
+exports.good_Greeting = goodGreeting;
+//export default goodGreeting;
+//export {badGreeting};
